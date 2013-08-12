b3bd4885127f6f47c65cb6840822e8a4a7f1fbfc
yihao <yhchinabest@gmail.com>
2013-08-13 01:40:25 +0800

    Test

--- OhMyfriends/src/com/findu/demo/manager/BaseMapManager.java
+++ OhMyfriends/src/com/findu/demo/manager/BaseMapManager.java
@@ -12,14 +12,14 @@ import com.findu.demo.location.LocationChangedListener;
 
 public abstract class BaseMapManager {
 	
-//	public abstract void setMapView(MapView mapview);
-//	
-//	public abstract MapView getMapView();
+	public abstract void setMapView(MapView mapview);
+	
+	public abstract MapView getMapView();
 	
 	//public abstract void mapInit(MainActivity context, MapView mapView);
 	public abstract void setLocationChangedListener(LocationChangedListener listener);
-//	public abstract void setMapController(MapController controller);
-//	public abstract MapController getMapController();
+	public abstract void setMapController(MapController controller);
+	public abstract MapController getMapController();
 	public abstract MKMapViewListener getMapViewListener();
 	public abstract void mapListenerInit(MapListenerCallback callback);
 	
