82bfaaf1c7690e043f9c607f334dfc9cefcc0dac
yihao <yhchinabest@gmail.com>
2013-08-13 01:13:35 +0800

    第一阶段重构
    MyMapManager更名为BaseMapManager，创建子类MapManager，并实现LocationChangedListener,
    ItemOverlayOnTapListener, RouteSearchListener
    将MyFriendsMain的大部分逻辑移到MapManager类中。
    
    下一阶段，将绘图相关的逻辑整合一下，如下段代码
    // 设定直接连接线样式
    Symbol mLineSymbol = new Symbol();
    Symbol.Color mLineColor = mLineSymbol.new Color();
    Graphic mLineGraphic;// 绘图专用
    GeoPoint[] mLinePoints = new GeoPoint[2];// 两点连接坐标
    
    // 构建步行路线连接线
    Geometry mWalkingGeometry = new Geometry();
    Symbol mWalkingSymbol = new Symbol();
    Symbol.Color mWalkingColor = mWalkingSymbol.new Color();
    Graphic mWalkingGraphic;// 步行连接线绘图专用

--- OhMyfriends/src/com/findu/demo/manager/BaseMapManager.java
+++ OhMyfriends/src/com/findu/demo/manager/BaseMapManager.java
@@ -10,16 +10,16 @@ import com.baidu.platform.comapi.basestruct.GeoPoint;
 import com.findu.demo.FriendsApplication;
 import com.findu.demo.location.LocationChangedListener;
 
-public abstract class MyMapManager {
+public abstract class BaseMapManager {
 	
-	public abstract void setMapView(MapView mapview);
-	
-	public abstract MapView getMapView();
+//	public abstract void setMapView(MapView mapview);
+//	
+//	public abstract MapView getMapView();
 	
 	//public abstract void mapInit(MainActivity context, MapView mapView);
 	public abstract void setLocationChangedListener(LocationChangedListener listener);
-	public abstract void setMapController(MapController controller);
-	public abstract MapController getMapController();
+//	public abstract void setMapController(MapController controller);
+//	public abstract MapController getMapController();
 	public abstract MKMapViewListener getMapViewListener();
 	public abstract void mapListenerInit(MapListenerCallback callback);
 	
